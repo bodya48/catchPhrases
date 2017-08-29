cd415bf4ff2b6e77713c62d336f1cafc91ae3d98
Bogdan <blaukhin@plexteq.com>
2017-08-29 23:50:08 +0300

    started project

---
+++ catchPhrases/catchPhrases/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  catchPhrases
+//
+//  Created by Bogdan Laukhin on 8/29/17.
+//  Copyright © 2017 ua.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ catchPhrases/catchPhrases/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  catchPhrases
+//
+//  Created by Bogdan Laukhin on 8/29/17.
+//  Copyright © 2017 ua.org. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
